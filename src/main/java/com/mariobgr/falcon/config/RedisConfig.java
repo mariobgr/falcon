f61dd0cecfcd6a9a0936b9ae7da61f3fec861453
mario <mariobgr@icloud.com>
2018-11-26 04:07:57 +0200

    Added redis password

--- src/main/java/com/mariobgr/falcon/config/RedisConfig.java
+++ src/main/java/com/mariobgr/falcon/config/RedisConfig.java
@@ -1,19 +1,28 @@
 package com.mariobgr.falcon.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableAutoConfiguration
 public class RedisConfig {
 
+    @Value("${spring.redis.password}")
+    private String redisPass;
+
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+    public JedisConnectionFactory jedisConnectionFactory() {
+
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
+        connectionFactory.setPassword(redisPass);
+
+        return connectionFactory;
     }
 
     @Bean
@@ -21,6 +30,8 @@ public class RedisConfig {
         final RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
         template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+        template.setKeySerializer(new StringRedisSerializer());
+
         return template;
     }
 
